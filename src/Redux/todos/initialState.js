6edfb0b9fab24c0d9716da273c82bfda0b91265b
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-11-22 00:01:49 +0600

    todo item working from redux initialstate

--- src/Redux/todos/initialState.js
+++ src/Redux/todos/initialState.js
@@ -1,13 +1,22 @@
 export const initialState = [{
     _id: 1,
-    completed: false,
+    completed: true,
     text: "This IS REACT REDUX "
 
 }, {
-    _id: 1,
-    completed: false,
-    text: "This IS REACT REDUX ",
+    _id: 2,
+    completed: true,
+    text: "This IS REACT REDUX 2 ",
     color: 'red'
 
 
-}]
+},
+{
+    _id: 3,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+
+
+}
+]
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
+  const todos = useSelector((state) => state.todos);
   return (
     <div class="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      <TodoItem />
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
     </div>
   );
 };
