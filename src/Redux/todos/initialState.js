a48e87a9da20b175eb2c5de0f57e52562131dea3
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-11-22 02:34:48 +0600

    22/11/2022 2:35AM  good night

--- src/Redux/todos/initialState.js
+++ src/Redux/todos/initialState.js
@@ -17,7 +17,95 @@ export const initialState = [{
     completed: false,
     text: "This IS REACT REDUX 2 ",
     color: 'red'
-
-
-}
+},
+{
+    _id: 4,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 5,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 6,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 7,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 8,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 9,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 10,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 11,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 12,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 13,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 14,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 15,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 16,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 17,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
+{
+    _id: 18,
+    completed: false,
+    text: "This IS REACT REDUX 2 ",
+    color: 'red'
+},
 ]
