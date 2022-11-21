56e8acd990b4ca362ee129fad7b435af7170311d
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-11-21 23:47:39 +0600

    redux complite setup

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
-
+import { Provider } from "react-redux";
+import store from "./Redux/store"
 function App() {
   return (
-    <div
-      class="grid place-items-center bg-blue-100 h-screen px-6 font-sans"
-    >
-      <Navbar />
+    <Provider store={store}>
+      <div
+        class="grid place-items-center bg-blue-100 h-screen px-6 font-sans"
+      >
+        <Navbar />
 
 
-      <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-        <Header />
+        <div class="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+          <Header />
 
-        <hr class="mt-4" />
+          <hr class="mt-4" />
 
-        <TodoList />
-        <hr class="mt-4" />
+          <TodoList />
+          <hr class="mt-4" />
 
 
-        <Footer />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
